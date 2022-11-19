183a36464b8e956dd667fa312a34fa3c3aa01b92
Md Somad <mdsomad10@gmail.com>
2022-11-19 11:26:37 +0530

    first commit

---
+++ lib/Home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("API Using"),
+      ),
+
+      body: Column(
+        children: [
+
+      ]),
+      
+    );
+  }
+}
